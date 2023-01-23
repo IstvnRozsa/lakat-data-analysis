620750ddd142e0383242f644f2b86a6f3a760cda
pistirozsa <pisti.rzsa@gmail.com>
2023-01-23 21:29:09 +0100

    Add lotka voltera

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import pylab as p
+from scipy import integrate
 
 def generate_labels_data(data_dict):
     # Data to plot
@@ -212,3 +214,59 @@ plt.ylabel("Revenue")
 title_6_6 = "The highest revenue was " + str(max_hourly_revenue["Price from File"].sum()) + " at " + max_hourly_revenue["Hour"].max()
 plt.title(title_6_6)
 plt.show()
+
+
+
+'''
+Task Lotka Voltera
+'''
+
+
+a = 1.
+b = 0.1
+c = 1.5
+d = 0.75
+def dX_dt(X, t=0):
+    """ Return the growth rate of fox and rabbit populations. """
+    return np.array([ a*X[0] -   b*X[0]*X[1] , -c*X[1] + d*b*X[0]*X[1] ])
+
+
+X_f0 = np.array([     0. ,  0.])
+X_f1 = np.array([ c/(d*b), a/b])
+all(dX_dt(X_f0) == np.zeros(2) ) and all(dX_dt(X_f1) == np.zeros(2)) # => True
+
+
+def d2X_dt2(X, t=0):
+    """ Return the Jacobian matrix evaluated in X. """
+    return np.array([[a -b*X[1],   -b*X[0]     ],
+                  [b*d*X[1] ,   -c +b*d*X[0]] ])
+
+A_f0 = d2X_dt2(X_f0)
+
+
+A_f1 = d2X_dt2(X_f1)                    # >>> array([[ 0.  , -2.  ],
+                                        #            [ 0.75,  0.  ]])
+# whose eigenvalues are +/- sqrt(c*a).j:
+lambda1, lambda2 = np.linalg.eigvals(A_f1) # >>> (1.22474j, -1.22474j)
+# They are imaginary numbers. The fox and rabbit populations are periodic as follows from further
+# analysis. Their period is given by:
+T_f1 = 2*np.pi/abs(lambda1)                # >>> 5.130199
+
+
+t = np.linspace(0, 15,  1000)              # time
+X0 = np.array([10, 5])                     # initials conditions: 10 rabbits and 5 foxes
+X, infodict = integrate.odeint(dX_dt, X0, t, full_output=True)
+infodict['message']                     # >>> 'Integration successful.'
+
+
+rabbits, foxes = X.T
+f1 = p.figure()
+p.plot(t, rabbits, 'r-', label='Rabbits')
+p.plot(t, foxes  , 'b-', label='Foxes')
+p.grid()
+p.legend(loc='best')
+p.xlabel('time')
+p.ylabel('population')
+p.title('Evolution of fox and rabbit populations')
+p.show()
+f1.savefig('rabbits_and_foxes_1.png')
